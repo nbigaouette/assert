d69e9c8d10dd423d684cd998e9a2b9ff9156a7f3
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-10-18 12:06:49 -0400

    Added Assert_isinf_isnan(), but now depends on stdcout.git

--- src/Assert.hpp
+++ src/Assert.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdlib>
 
+#include <StdCout.hpp>
+
 #define DEBUGP(x)  std_cout << __FILE__ << ":" << __LINE__ << ":\n    " << x;
 // Quote something, usefull to quote a macro's value
 #define _QUOTEME(x) #x
@@ -12,6 +14,7 @@
 // http://stackoverflow.com/questions/389180/assert-and-ndebug
 // #ifdef YDEBUG
 
+// Undefine assert() to use our own
 #ifdef assert
 #undef assert
 #endif
@@ -34,7 +37,25 @@
 //     else   // This 'else' exists to catch the user's following semicolon
 // #endif // #ifdef YDEBUG
 
-
+// **************************************************************
+template <class T>
+inline void Assert_isinf_isnan(T value)
+{
+#ifndef __SUNPRO_CC
+//     if (isinf(value))
+//     {
+//         std_cout << "value is inf!!! value = " << value << "\n";
+//         std_cout << "Aborting\n";
+//     }
+    assert(!isinf(value));
+#endif // #ifndef __SUNPRO_CC
+//     if (isnan(value))
+//     {
+//         std_cout << "value is NaN!!! value = " << value << "\n";
+//         std_cout << "Aborting\n";
+//     }
+    assert(!isnan(value));
+}
 
 #endif // INC_ASSERT_hpp
 
