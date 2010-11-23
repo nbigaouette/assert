c1a7504357b5caee9b19a1291327dcb8b755990a
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-11-23 10:47:24 -0500

    Bypass stupid intel compiler which have problem with isnan() and isinf(). isnan() is easy to replace, not isinf().

--- src/Assert.hpp
+++ src/Assert.hpp
@@ -66,6 +66,7 @@ inline void Assert_isinf_isnan(T value)
     assert(!std::isnan(value));
 }
 */
+
 #ifdef __INTEL_COMPILER
 #define Assert_isinf_isnan(value)                                   \
 {                                                                   \
@@ -81,6 +82,19 @@ inline void Assert_isinf_isnan(T value)
 #else // #ifdef __INTEL_COMPILER
 #define Assert_isinf_isnan(value)                                   \
 {                                                                   \
+    if (std::isinf((value)))                                        \
+    {                                                               \
+        std_cout << "value is inf!!! value = " << (value) << "\n";  \
+        std_cout << "Aborting\n";                                   \
+    }                                                               \
+    assert(!std::isinf((value)));                                   \
+                                                                    \
+    if (std::isnan((value)))                                        \
+    {                                                               \
+        std_cout << "value is NaN!!! value = " << (value) << "\n";  \
+        std_cout << "Aborting\n";                                   \
+    }                                                               \
+    assert(!std::isnan((value)));                                   \
 }
 #endif // #ifdef __INTEL_COMPILER
 
